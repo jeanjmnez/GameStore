c1c77b116a7f4c8bf217220ab16bd5016604c2ca
Jean Jimenez <jeanjmnez@gmail.com>
2024-04-25 15:14:13 -0400

    Add files via upload

---
+++ GameStore.Fontend/Clients/GamesClient.cs
@@ -0,0 +1,39 @@
+﻿using GameStore.Fontend.Models;
+
+namespace GameStore.Fontend.Clients
+{
+    public class GamesClient
+    {
+        private readonly List<GameSummary> games =
+        [
+            new (){
+                    Id= 1,
+                    Name="Stret Fighter II",
+                    Genre="Fighting",
+                    Price = 19.9M,
+                    ReleaseDate = new DateOnly(1992, 7,15)
+
+                },
+            new (){
+                Id= 1,
+                Name="Final Fantasy",
+                Genre="Roleplaying",
+                Price = 69.9M,
+                ReleaseDate = new DateOnly(2022, 9,27)
+
+            },
+            new (){
+                Id= 1,
+                Name="Fifa 23",
+                Genre="Sports",
+                Price = 69.9M,
+                ReleaseDate = new DateOnly(2022, 9,27)
+
+            }
+        ];
+
+
+        public GameSummary[] GetGames() => [.. games];
+
+    }
+}
---
+++ GameStore.Fontend/Models/GameDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace GameStore.Fontend.Models
+{
+    public class GameDetails
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? GenreId { get; set; }
+        public decimal Price { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+    }
+
+
+}
