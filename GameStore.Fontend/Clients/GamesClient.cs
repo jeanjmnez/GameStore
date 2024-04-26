946dd6e807ee83a7cc11435f23541dc3fd4fb251
Jean Carlos <jeanjmnez@gmail.com>
2024-04-25 21:16:56 -0400

    Adding the navigation and edit

--- GameStore.Fontend/Clients/GamesClient.cs
+++ GameStore.Fontend/Clients/GamesClient.cs
@@ -6,34 +6,59 @@ namespace GameStore.Fontend.Clients
     {
         private readonly List<GameSummary> games =
         [
-            new (){
-                    Id= 1,
-                    Name="Stret Fighter II",
-                    Genre="Fighting",
-                    Price = 19.9M,
-                    ReleaseDate = new DateOnly(1992, 7,15)
-
-                },
-            new (){
-                Id= 1,
-                Name="Final Fantasy",
-                Genre="Roleplaying",
-                Price = 69.9M,
-                ReleaseDate = new DateOnly(2022, 9,27)
+            new()
+            {
+                Id = 1,
+                Name = "Stret Fighter II",
+                Genre = "Fighting",
+                Price = 19.9M,
+                ReleaseDate = new DateOnly(1992, 7, 15)
 
             },
-            new (){
-                Id= 1,
-                Name="Fifa 23",
-                Genre="Sports",
+            new()
+            {
+                Id = 1,
+                Name = "Final Fantasy",
+                Genre = "Roleplaying",
                 Price = 69.9M,
-                ReleaseDate = new DateOnly(2022, 9,27)
+                ReleaseDate = new DateOnly(2022, 9, 27)
+
+            },
+            new()
+            {
+                Id = 1,
+                Name = "Fifa 23",
+                Genre = "Sports",
+                Price = 69.9M,
+                ReleaseDate = new DateOnly(2022, 9, 27)
 
             }
         ];
 
 
+        private readonly Genre[] genres = new GenresClient().GetGenres();
+
+
         public GameSummary[] GetGames() => [.. games];
+
+        public void AddGame(GameDetails game)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId); 
+            var genre = genres.Single(genre =>genre.Id == int.Parse(game.GenreId));
+
+            var gameSummary = new GameSummary
+            {
+                Id = games.Count + 1,
+                Name = game.Name,
+                Genre = genre.Name,
+                Price = game.Price,
+                ReleaseDate = game.ReleaseDate
+            };
+
+            games.Add(gameSummary);
+        }
+
+
 
     }
 }
---
+++ GameStore.Fontend/Clients/GenresClient.cs
@@ -0,0 +1,39 @@
+﻿using GameStore.Fontend.Models;
+
+namespace GameStore.Fontend.Clients
+{
+    public class GenresClient
+    {
+        private readonly Genre[] genres = [
+            new()
+            {
+                Id = 1,
+                Name = "Fighting"
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Roleplaying"
+            },
+            new()
+            {
+                Id = 3,
+                Name = "Sports"
+            },
+            new()
+            {
+                Id = 4,
+                Name = "Racing"
+            },
+            new()
+            {
+                Id = 5,
+                Name = "Kids and Familiy"
+            }
+        ];
+
+
+        public Genre[] GetGenres() => genres;
+    }
+
+}
