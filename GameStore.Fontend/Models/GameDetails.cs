1ca048665c960eb337c603f196d4b250fc167872
Jean Carlos <jeanjmnez@gmail.com>
2024-04-25 21:23:53 -0400

    Adding validation

--- GameStore.Fontend/Models/GameDetails.cs
+++ GameStore.Fontend/Models/GameDetails.cs
@@ -1,10 +1,19 @@
-﻿namespace GameStore.Fontend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Fontend.Models
 {
     public class GameDetails
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public required string Name { get; set; }
+        
+        [Required]
         public string? GenreId { get; set; }
+
+        [Range(1, 100)]
         public decimal Price { get; set; }
         public DateOnly ReleaseDate { get; set; }
     }
